d9110b54bb51ebb92787667d045ef93c5a55432a
Boris <borisgerard333@gmail.com>
2021-11-12 12:28:59 -0500

    another one

--- src/main/java/api/quickpolls/conttollers/ComputeResultController.java
+++ src/main/java/api/quickpolls/conttollers/ComputeResultController.java
@@ -1,18 +1,10 @@
 package api.quickpolls.conttollers;
 
-import api.quickpolls.domains.Vote;
-import api.quickpolls.repositories.VoteRepository;
-import api.quickpolls.resources.OptionCount;
-import api.quickpolls.resources.VoteResult;
 import api.quickpolls.services.ResultService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class ComputeResultController {
