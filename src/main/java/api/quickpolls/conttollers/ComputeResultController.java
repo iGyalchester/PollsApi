6d34b43fb8e33c16bec1c314ac0ac69cb7788120
Boris <borisgerard333@gmail.com>
2021-11-12 12:21:36 -0500

    new commit

--- src/main/java/api/quickpolls/conttollers/ComputeResultController.java
+++ src/main/java/api/quickpolls/conttollers/ComputeResultController.java
@@ -4,12 +4,11 @@ import api.quickpolls.domains.Vote;
 import api.quickpolls.repositories.VoteRepository;
 import api.quickpolls.resources.OptionCount;
 import api.quickpolls.resources.VoteResult;
+import api.quickpolls.services.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import java.util.HashMap;
@@ -18,30 +17,11 @@ import java.util.Map;
 @RestController
 public class ComputeResultController {
 
-    @Inject
-    private VoteRepository voteRepository;
+    @Autowired
+    ResultService resultService;
 
-    @RequestMapping(value="/computeresult", method= RequestMethod.GET)
+    @GetMapping(value="/computeresult")
     public ResponseEntity<?> computeResult(@RequestParam Long pollId) {
-        VoteResult voteResult = new VoteResult();
-        Iterable<Vote> allVotes = voteRepository.findByPoll(pollId);
-        // Algorithm to count votes
-        int totalVotes = 0;
-        Map<Long, OptionCount> tempMap = new HashMap<Long, OptionCount>();
-        for(Vote v : allVotes) {
-            totalVotes ++;
-            // Get the OptionCount corresponding to this Option
-            OptionCount optionCount = tempMap.get(v.getOption().getOptionId());
-            if(optionCount == null) {
-                optionCount = new OptionCount();
-                optionCount.setOptionId(v.getOption().getOptionId());
-                tempMap.put(v.getOption().getOptionId(), optionCount);
-            }
-            optionCount.setCount(optionCount.getCount()+1);
-        }
-        voteResult.setTotalVotes(totalVotes);
-        voteResult.setResults(tempMap.values());
-
-        return new ResponseEntity<>(voteResult, HttpStatus.OK);
+        return resultService.computeResult(pollId);
     }
 }
--- src/main/java/api/quickpolls/conttollers/VoteController.java
+++ src/main/java/api/quickpolls/conttollers/VoteController.java
@@ -2,16 +2,12 @@ package api.quickpolls.conttollers;
 
 
 import api.quickpolls.domains.Vote;
-import api.quickpolls.repositories.VoteRepository;
+
 import api.quickpolls.services.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.inject.Inject;
 
 @RestController
 public class VoteController {
