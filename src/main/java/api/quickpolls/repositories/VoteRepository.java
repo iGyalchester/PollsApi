34fb5e5ab41716e747235e515d9b22e3ce6d5921
Boris <borisgerard333@gmail.com>
2021-11-12 15:37:03 -0500

    another one

--- src/main/java/api/quickpolls/repositories/VoteRepository.java
+++ src/main/java/api/quickpolls/repositories/VoteRepository.java
@@ -3,7 +3,9 @@ package api.quickpolls.repositories;
 import api.quickpolls.domains.Vote;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VoteRepository extends CrudRepository<Vote, Long> {
 
     @Query(value="select v.* from POption o, Vote v where o.POLL_ID = ?1 and v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
